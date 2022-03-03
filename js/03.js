15e30fb76aaec82413ea9524f5607b5a35e5c974
EmilHeilbo <19676610+EmilHeilbo@users.noreply.github.com>
2022-03-03 03:01:43 +0100

    refactor: clean up 03.js, improve checkInput

--- js/03.js
+++ js/03.js
@@ -7,19 +7,18 @@ var licensePlate = {
     licensePlate.input.addEventListener('input', licensePlate.checkInput);
   },
 
-  checkInput: (e) => {
-    const REGEX = /^[A-ZÆØÅ]{1,2}\d{0,5}$/g
-    const str = licensePlate.input.value.toUpperCase();
-    var result = null;
-    if (str.length < 2 && !str.match(REGEX))
-      result = str.substring(0, str.length - 1);
-    else if (str.length <= 7 && !str.match(REGEX))
-      result = str.substring(0, str.length - 1);
-    else if (str.length > 7)
-      result = str.substring(0, 7);
-    licensePlate.input.value = result ?? str;
-    licensePlate.input.setAttribute('style', `background-color: ${(result != null) ? 'thistle' : 'lightcyan'}`);
-    e.preventDefault();
+  checkInput: (e) => { e.preventDefault();
+    const REGEX = /^[A-ZÆØÅ]{1,2}\d{0,5}$/g;
+    const INPUT = licensePlate.input.value.toUpperCase();
+    let str = INPUT;
+    while (!str.match(REGEX) && str != "")
+      if (str.length <= 7)
+        str = str.substring(0, str.length - 1);
+      else
+        str = str.substring(0, 7);
+    licensePlate.input.value = str;
+    licensePlate.input.setAttribute('style',
+        `background-color: ${(str != INPUT) ? 'thistle' : 'lightcyan'}`);
   } 
 };
 
